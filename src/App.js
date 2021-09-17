cc828c275047f4cb7f545fa0fde72737f66257ac
tepatti <teemu.makela99@hotmail.com>
2021-09-17 21:36:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          <code>Moi Antti!</code>
+        </h1>
+        <p>Rakastan sua</p>
       </header>
     </div>
   );
